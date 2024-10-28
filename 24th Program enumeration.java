1cb50f3d8f2a6953a9f49fad3ec7cdd30b6800e5
MD Mahfuzur Rahman Siam <ksiam3409@gmail.com>
2024-10-28 07:01:04 +0600

    Add files via upload

---
+++ 24th Program enumeration.java
@@ -0,0 +1,29 @@
+switch(this) {
+    case SMALL:
+      return "small";
+
+    case MEDIUM:
+      return "medium";
+
+    case LARGE:
+      return "large";
+
+    case EXTRALARGE:
+      return "extra large";
+
+    default:
+      return null;
+     }
+  }
+
+  public static void main(String[] args) {
+
+     // call the method getSize()
+     // using the object SMALL
+     System.out.println("The size of Pizza I get is " + Size.SMALL.getSize());
+
+     // call the method getSize()
+     // using the object LARGE
+     System.out.println("The size of Pizza I want is " + Size.LARGE.getSize());
+  }
+}
